a5e0b020cf702da6a8ccbf07ddb7d8b68971ec43
RMerkulov <fotoromario@gmail.com>
2023-06-23 17:54:00 +0300

    upd

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import { signIn, signOut, useSession } from "next-auth/react";
 
 const NavBar = () => {
+  //check auth
   const session = useSession();
 
   return (
